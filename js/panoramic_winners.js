4c04de28a76117eb23d9142e1743d9f4baac4840
lihao <805459342@qq.com>
2026-01-17 11:52:58 +0800

    fix: task1数据渲染

--- js/panoramic_winners.js
+++ js/panoramic_winners.js
@@ -1,152 +1,185 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
     const winnersData = [
         {
-            team: "ChohoTech",
-            profileLink: "https://www.codabench.org/profiles/organization/419/",
+            team: "zhiqin1998",
+            profileLink: "#",
             metrics: {
-                image: { dsc: "0.918", iou: "0.849", nsd: "0.956" },
-                instance: { dsc: "0.845", iou: "0.765", nsd: "0.872", ia: "0.883" },
-                algo: { rt: "13.291", gpu: "&nbsp;7341.120" }
+                image: {
+                    dsc: { mean: "0.9176", pm: "0.1970" },
+                    iou: { mean: "0.8822", pm: "0.1924" },
+                    nsd: { mean: "0.9487", pm: "0.2042" }
+                },
+                instance: {
+                    dsc: { mean: "0.6668", pm: "0.1788" },
+                    iou: { mean: "0.5513", pm: "0.1589" },
+                    nsd: { mean: "0.8838", pm: "0.2058" },
+                    ia:  { mean: "0.5778", pm: "0.2589" }
+                }
             },
             rank: 1,
             links: {
-                ppt: { url: "files/2d/01_ChohoTech.pdf", icon: "icons/ppt.png", name: "PPT Share" },
-                code: { url: "https://github.com/fenglian425/STS2D", icon: "icons/github.png", name: "Code" },
-                paper: { url: "#", icon: "icons/openreview.png", name: "Paper", disabled: true }
-            }
-        },
-        {
-            team: "camerart2024",
-            profileLink: "https://www.codabench.org/profiles/user/camerart2024/",
-            metrics: {
-                image: { dsc: "0.869", iou: "0.771", nsd: "0.907" },
-                instance: { dsc: "0.703", iou: "0.613", nsd: "0.740", ia: "0.734" },
-                algo: { rt: "13.274", gpu: "14250.980" }
-            },
-            rank: 2,
-            links: {
-                ppt: { url: "files/2d/02_camerart2024.pdf", icon: "icons/ppt.png", name: "PPT Share" },
-                code: { url: "https://github.com/Liaaaar/2024-MICCAI-STS-2D", icon: "icons/github.png", name: "Code" },
-                paper: { url: "#", icon: "icons/openreview.png", name: "Paper", disabled: true }
+                code: {
+                    url: "https://github.com/ricoleehduu/STS-Challenge-2025/blob/main/code/task1/rank01-Tan%20Zhi%20Qin.zip",
+                    icon: "icons/github.png",
+                    name: "Code"
+                },
+                paper: {
+                    url: "https://github.com/ricoleehduu/STS-Challenge-2025/blob/main/TechnicalReport/task1/rank01-21_U_Mamba2_SSL_for_Semi_Super.pdf",
+                    icon: "icons/openreview.png",
+                    name: "Paper"
+                }
             }
         },
         {
             team: "jichangkai",
-            profileLink: "https://www.codabench.org/profiles/user/jichangkai/",
+            profileLink: "#",
             metrics: {
-                image: { dsc: "0.915", iou: "0.859", nsd: "0.944" },
-                instance: { dsc: "0.801", iou: "0.734", nsd: "0.817", ia: "0.832" },
-                algo: { rt: "55.897", gpu: "25461.260" }
+                image: {
+                    dsc: { mean: "0.9431", pm: "0.0183" },
+                    iou: { mean: "0.8929", pm: "0.0314" },
+                    nsd: { mean: "0.9934", pm: "0.0089" }
+                },
+                instance: {
+                    dsc: { mean: "0.6268", pm: "0.0940" },
+                    iou: { mean: "0.4994", pm: "0.0824" },
+                    nsd: { mean: "0.8786", pm: "0.1114" },
+                    ia:  { mean: "0.4748", pm: "0.1690" }
+                }
+            },
+            rank: 2,
+            links: {
+                code: {
+                    url: "https://github.com/ricoleehduu/STS-Challenge-2025/blob/main/code/task1/rank02-changkaiJi.zip",
+                    icon: "icons/github.png",
+                    name: "Code"
+                },
+                paper: {
+                    url: "https://github.com/ricoleehduu/STS-Challenge-2025/blob/main/TechnicalReport/task1/rank02-19_Efficient_nnU_Net_for_Tooth.pdf",
+                    icon: "icons/openreview.png",
+                    name: "Paper"
+                }
+            }
+        },
+        {
+            team: "DiceMed",
+            profileLink: "#",
+            metrics: {
+                image: {
+                    dsc: { mean: "0.8478", pm: "0.2397" },
+                    iou: { mean: "0.7862", pm: "0.2498" },
+                    nsd: { mean: "0.8530", pm: "0.2623" }
+                },
+                instance: {
+                    dsc: { mean: "0.6019", pm: "0.1798" },
+                    iou: { mean: "0.4815", pm: "0.1532" },
+                    nsd: { mean: "0.8110", pm: "0.2326" },
+                    ia:  { mean: "0.5099", pm: "0.2486" }
+                }
             },
             rank: 3,
             links: {
-                ppt: { url: "files/2d/03_jichangkai.pdf", icon: "icons/ppt.png", name: "PPT Share" },
+                code: {
+                    url: "https://github.com/ricoleehduu/STS-Challenge-2025/blob/main/code/task1/rank03-dicemed.zip",
+                    icon: "icons/github.png",
+                    name: "Code"
+                },
+                paper: {
+                    url: "https://github.com/ricoleehduu/STS-Challenge-2025/blob/main/TechnicalReport/task1/rank03-23_nnUNet_for_Semi_supervised_.pdf",
+                    icon: "icons/openreview.png",
+                    name: "Paper"
+                }
+            }
+        },
+        {
+            team: "cccc2024",
+            profileLink: "#",
+            metrics: {
+                image: {
+                    dsc: { mean: "0.4521", pm: "0.4823" },
+                    iou: { mean: "0.4344", pm: "0.4636" },
+                    nsd: { mean: "0.4704", pm: "0.5017" }
+                },
+                instance: {
+                    dsc: { mean: "0.3052", pm: "0.3315" },
+                    iou: { mean: "0.2477", pm: "0.2699" },
+                    nsd: { mean: "0.4156", pm: "0.4502" },
+                    ia:  { mean: "0.2509", pm: "0.2918" }
+                }
+            },
+            rank: 4,
+            links: {
+                code: { url: "#", icon: "icons/github.png", name: "Code", disabled: true },
+                paper: {
+                    url: "https://github.com/ricoleehduu/STS-Challenge-2025/blob/main/TechnicalReport/task1/rank04-25_TCM_UNet_A_U_Net_with_Tri_A.pdf",
+                    icon: "icons/openreview.png",
+                    name: "Paper"
+                }
+            }
+        },
+        {
+            team: "minh_dang",
+            profileLink: "#",
+            metrics: {
+                image: {
+                    dsc: { mean: "0.8105", pm: "0.0904" },
+                    iou: { mean: "0.6902", pm: "0.1184" },
+                    nsd: { mean: "0.8666", pm: "0.0915" }
+                },
+                instance: {
+                    dsc: { mean: "0.1464", pm: "0.0273" },
+                    iou: { mean: "0.1135", pm: "0.0268" },
+                    nsd: { mean: "0.1964", pm: "0.0344" },
+                    ia:  { mean: "0.1131", pm: "0.0691" }
+                }
+            },
+            rank: 5,
+            links: {
                 code: { url: "#", icon: "icons/github.png", name: "Code", disabled: true },
                 paper: { url: "#", icon: "icons/openreview.png", name: "Paper", disabled: true }
             }
         },
         {
-            team: "dew123",
-            profileLink: "https://www.codabench.org/profiles/user/dew123/",
+            team: "GUETIICI",
+            profileLink: "#",
             metrics: {
-                image: { dsc: "0.844", iou: "0.736", nsd: "0.886" },
-                instance: { dsc: "0.224", iou: "0.574", nsd: "0.699", ia: "0.658" },
-                algo: { rt: "13.906", gpu: "15088.500" }
+                image: {
+                    dsc: { mean: "0.8660", pm: "0.1160" },
+                    iou: { mean: "0.7779", pm: "0.1425" },
+                    nsd: { mean: "0.9352", pm: "0.1078" }
+                },
+                instance: {
+                    dsc: { mean: "0.0043", pm: "0.0020" },
+                    iou: { mean: "0.0022", pm: "0.0010" },
+                    nsd: { mean: "0.0232", pm: "0.0096" },
+                    ia:  { mean: "0.0000", pm: "0.0000" }
+                }
             },
-            rank: 4,
+            rank: 6,
             links: {
-                ppt: { url: "files/2d/04_dew123.pptx", icon: "icons/ppt.png", name: "PPT Share" },
-                code: { url: "https://github.com/Dew026/DICL", icon: "icons/github.png", name: "Code" },
+                code: { url: "#", icon: "icons/github.png", name: "Code", disabled: true },
                 paper: { url: "#", icon: "icons/openreview.png", name: "Paper", disabled: true }
             }
         },
         {
             team: "junqiangmler",
-            profileLink: "https://www.codabench.org/profiles/user/junqiangmler/",
+            profileLink: "#",
             metrics: {
-                image: { dsc: "0.826", iou: "0.713", nsd: "0.867" },
-                instance: { dsc: "0.368", iou: "0.545", nsd: "0.678", ia: "0.552" },
-                algo: { rt: "14.047", gpu: "12483.380" }
-            },
-            rank: 5,
-            links: {
-                ppt: { url: "#", icon: "icons/ppt.png", name: "PPT Share", disabled: true },
-                code: { url: "https://github.com/junqiangchen/STS2024", icon: "icons/github.png", name: "Code" },
-                paper: { url: "#", icon: "icons/openreview.png", name: "Paper", disabled: true }
-            }
-        },
-        {
-            team: "isjinghao",
-            profileLink: "https://www.codabench.org/profiles/user/isjinghao/",
-            metrics: {
-                image: { dsc: "0.826", iou: "0.730", nsd: "0.863" },
-                instance: { dsc: "0.750", iou: "0.587", nsd: "0.676", ia: "0.698" },
-                algo: { rt: "21.134", gpu: "27987.900" }
-            },
-            rank: 6,
-            links: {
-                ppt: { url: "files/2d/06_isjinghao.pdf", icon: "icons/ppt.png", name: "PPT Share" },
-                code: { url: "https://github.com/isbrycee/SemiT-SAM", icon: "icons/github.png", name: "Code" },
-                paper: { url: "#", icon: "icons/openreview.png", name: "Paper", disabled: true }
-            }
-        },
-        {
-            team: "lazyman",
-            profileLink: "https://www.codabench.org/profiles/user/lazyman/",
-            metrics: {
-                image: { dsc: "0.597", iou: "0.430", nsd: "0.870" },
-                instance: { dsc: "0.385", iou: "0.347", nsd: "0.725", ia: "0.086" },
-                algo: { rt: "11.810", gpu: "13910.320" }
+                image: {
+                    dsc: { mean: "0.3686", pm: "0.4548" },
+                    iou: { mean: "0.3419", pm: "0.4220" },
+                    nsd: { mean: "0.3981", pm: "0.4911" }
+                },
+                instance: {
+                    dsc: { mean: "0.0899", pm: "0.1149" },
+                    iou: { mean: "0.0766", pm: "0.0984" },
+                    nsd: { mean: "0.1075", pm: "0.1370" },
+                    ia:  { mean: "0.0875", pm: "0.1176" }
+                }
             },
             rank: 7,
             links: {
-                ppt: { url: "files/2d/07_lazyman.pptx", icon: "icons/ppt.png", name: "PPT Share" },
                 code: { url: "#", icon: "icons/github.png", name: "Code", disabled: true },
-                paper: { url: "#", icon: "icons/openreview.png", name: "Paper", disabled: true }
-            }
-        },
-        {
-            team: "caiyichen",
-            profileLink: "https://www.codabench.org/profiles/user/caiyichen/",
-            metrics: {
-                image: { dsc: "0.820", iou: "0.703", nsd: "0.855" },
-                instance: { dsc: "0.530", iou: "0.492", nsd: "0.566", ia: "0.574" },
-                algo: { rt: "19.531", gpu: "26666.571" }
-            },
-            rank: 8,
-            links: {
-                ppt: { url: "#", icon: "icons/ppt.png", name: "PPT Share", disabled: true },
-                code: { url: "#", icon: "icons/github.png", name: "Code", disabled: true },
-                paper: { url: "#", icon: "icons/openreview.png", name: "Paper", disabled: true }
-            }
-        },
-        {
-            team: "guo77777",
-            profileLink: "https://www.codabench.org/profiles/user/guo77777/",
-            metrics: {
-                image: { dsc: "0.755", iou: "0.614", nsd: "0.803" },
-                instance: { dsc: "0.324", iou: "0.285", nsd: "0.386", ia: "0.279" },
-                algo: { rt: "18.421", gpu: "19694.260" }
-            },
-            rank: 9,
-            links: {
-                ppt: { url: "#", icon: "icons/ppt.png", name: "PPT Share", disabled: true },
-                code: { url: "#", icon: "icons/github.png", name: "Code", disabled: true },
-                paper: { url: "#", icon: "icons/openreview.png", name: "Paper", disabled: true }
-            }
-        },
-        {
-            team: "cccc2024",
-            profileLink: "https://www.codabench.org/profiles/organization/465/",
-            metrics: {
-                image: { dsc: "0.651", iou: "0.492", nsd: "0.679" },
-                instance: { dsc: "0.319", iou: "0.192", nsd: "0.218", ia: "0.244" },
-                algo: { rt: "13.462", gpu: "17730.480" }
-            },
-            rank: 10,
-            links: {
-                ppt: { url: "files/2d/10_cccc2024.pptx", icon: "icons/ppt.png", name: "PPT Share" },
-                code: { url: "https://github.com/SUTAN-01/MICCAI-STS-2024", icon: "icons/github.png", name: "Code" },
                 paper: { url: "#", icon: "icons/openreview.png", name: "Paper", disabled: true }
             }
         }
@@ -154,20 +187,29 @@ $(document).ready(function() {
 
     const $tbody = $('#winner-table-body');
 
+    function renderMetric(metric) {
+        if (!metric || !metric.mean) return "-";
+        return `${metric.mean} (±${metric.pm})`;
+    }
+
     winnersData.forEach((data, index) => {
+
         const isLast = index === winnersData.length - 1;
         const rowClass = isLast ? '' : 'winner-bottom-border';
         const rowStyle = isLast ? 'border-bottom: 2px solid black' : '';
 
-        // Generate links HTML
         const linksHtml = Object.entries(data.links).map(([key, link]) => {
-            let type = key;
-            if (key === 'code') type = 'github';
-            if (key === 'paper') type = 'openreview';
-
+            const type = key === 'code' ? 'github' : 'openreview';
             const liClass = link.disabled ? 'class="winner-disabled-link"' : '';
+            const href = link.disabled ? 'javascript:void(0)' : link.url;
+
             return `
-                <li ${liClass}>[<a href="${link.url}" class="winner-${type}-link"><img src="${link.icon}" class="winner-${type}-icon" alt="${link.name}">${link.name}</a>]</li>
+                <li ${liClass}>
+                    [<a href="${href}" class="winner-${type}-link">
+                        <img src="${link.icon}" class="winner-${type}-icon" alt="${link.name}">
+                        ${link.name}
+                    </a>]
+                </li>
             `;
         }).join('');
 
@@ -175,18 +217,20 @@ $(document).ready(function() {
             <tr class="${rowClass}" style="${rowStyle}">
                 <td class="winner-right-border">
                     <a href="${data.profileLink}" class="winner-user-link">
-                        <img src="icons/user.png" class="winner-user-icon" alt="User">${data.team}
+                        <img src="icons/user.png" class="winner-user-icon" alt="User">
+                        ${data.team}
                     </a>
                 </td>
-                <td>${data.metrics.image.dsc}</td>
-                <td>${data.metrics.image.iou}</td>
-                <td class="winner-right-border">${data.metrics.image.nsd}</td>
-                <td>${data.metrics.instance.dsc}</td>
-                <td>${data.metrics.instance.iou}</td>
-                <td>${data.metrics.instance.nsd}</td>
-                <td class="winner-right-border">${data.metrics.instance.ia}</td>
-                <td>${data.metrics.algo.rt}</td>
-                <td class="winner-right-border">${data.metrics.algo.gpu}</td>
+
+                <td>${renderMetric(data.metrics.image.dsc)}</td>
+                <td>${renderMetric(data.metrics.image.iou)}</td>
+                <td class="winner-right-border">${renderMetric(data.metrics.image.nsd)}</td>
+
+                <td>${renderMetric(data.metrics.instance.dsc)}</td>
+                <td>${renderMetric(data.metrics.instance.iou)}</td>
+                <td>${renderMetric(data.metrics.instance.nsd)}</td>
+                <td class="winner-right-border">${renderMetric(data.metrics.instance.ia)}</td>
+
                 <td class="winner-right-border">${data.rank}</td>
                 <td>
                     <ol class="winner-table-links">
@@ -195,6 +239,8 @@ $(document).ready(function() {
                 </td>
             </tr>
         `;
+
         $tbody.append(tr);
     });
+
 });
